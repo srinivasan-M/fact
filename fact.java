366aea5ac74813acd93d48b28e473cc8c96429f0
srinivasan-M <srinivasanksr15@gmail.com>
2017-04-18 20:54:28 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -4,6 +4,11 @@ public static void main(String args[])
 {
 int fact=1,i;
 int n=Integer.parseInt(args[0]);
+  if(n==0)
+    System.out.print("1");
+  else if(n<0)
+    System.out.print("Number should be a positive");
+  else
 for(i=1;i<=n;i++)
 {
 fact=fact*i;
